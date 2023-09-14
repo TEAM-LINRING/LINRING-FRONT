1c20e3201db2de5968a0ae4914024bd3e25d290c
Jiwon Choi <jwchoi@lablup.com>
2023-09-15 02:33:43 +0900

    feat: `home` 변경 및 MainScreen AppBar UI 구현
    - App의 entry point를 MainScreen()으로 변경

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "LINRING",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          color: Colors.black,
+          onPressed: () => {
+            Navigator.of(context).pop(),
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        actions: [
+          IconButton(
+            color: Colors.black,
+            onPressed: () {},
+            icon: const Icon(Icons.notifications),
+          )
+        ],
+      ),
+    );
+  }
+}
