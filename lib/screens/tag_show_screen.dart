a43e8acabcf8bacd1938356dcdd7e75f857af6b9
Jiwon Choi <jwchoi@lablup.com>
2023-10-10 11:35:36 +0900

    feat: `carousel_slider`를 사용하여 임시 UI 변경

--- lib/screens/tag_show_screen.dart
+++ lib/screens/tag_show_screen.dart
@@ -1,7 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
-final _controller = PageController(initialPage: 0);
-List<bool> _selected = [true, false];
+import 'package:linring_front_flutter/widgets/custom_appbar.dart';
 
 class TagShowScreen extends StatefulWidget {
   const TagShowScreen({Key? key}) : super(key: key);
@@ -13,127 +12,70 @@ class TagShowScreen extends StatefulWidget {
 class _TagShowScreenState extends State<TagShowScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _buildTabItem(0, '활성화'),
-              _buildTabItem(1, '비활성화'),
-            ],
-          ),
-        ),
-        Expanded(
-          child: PageView(
-            controller: _controller,
-            children: const [
-              TagActive(),
-              TagInactive(),
-            ],
-            onPageChanged: (index) {
-              setState(() {
-                for (int i = 0; i < _selected.length; i++) {
-                  _selected[i] = i == index;
-                }
-              });
-            },
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildTabItem(int index, String label) {
-    return InkWell(
-      splashColor: Colors.transparent,
-      highlightColor: Colors.transparent,
-      onTap: () {
-        setState(() {
-          if (index == 1) {
-            _controller.nextPage(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut);
-          } else {
-            _controller.previousPage(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut);
-          }
-          for (int i = 0; i < _selected.length; i++) {
-            _selected[i] = i == index;
-          }
-        });
-      },
-      child: Container(
-        width: 180,
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        decoration: BoxDecoration(
-          color: const Color(0xfffff6f4),
-          border: Border(
-            bottom: BorderSide(
-              color: _selected[index]
-                  ? const Color(0xff41350a).withOpacity(0.62)
-                  : Colors.transparent,
-              width: 2.0,
-            ),
-          ),
-        ),
-        child: Center(
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 24,
-              color: _selected[index]
-                  ? const Color(0xff1c1c1c)
-                  : const Color(0xffc8c8c8),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TagActive extends StatelessWidget {
-  const TagActive({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xfffff6f4),
-      ),
+      color: const Color(0xfffff6f4),
       child: Column(
         children: [
-          const SizedBox(height: 20),
-          GestureDetector(
-              onTap: () => Navigator.pushNamed(context, "/add"),
-              child: Card(
-                child: SizedBox(
-                  width: 360,
-                  height: 120,
-                  child: Center(
-                    child: Image.asset(
-                      "assets/icons/add_circle.png",
-                      width: 42,
+          CarouselSlider(
+            options: CarouselOptions(
+                height: 306.0,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 5)),
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(color: Color(0xff3a3a3a)),
+                    child: Text(
+                      'text $i',
+                      style: const TextStyle(fontSize: 16.0),
                     ),
-                  ),
+                  );
+                },
+              );
+            }).toList(),
+          ),
+          Column(
+            children: [
+              const Text("어떤 친구를 만나게 될까요?"),
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 300.0,
+                  enableInfiniteScroll: false,
                 ),
-              ))
+                items: [1, 2, 3, 4, 5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return const Card(
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Text("북악관에서"),
+                            Text("선배랑"),
+                            Text("스터디하기"),
+                            Text("같이 카페에서 각자 공부할 사람 구해요!"),
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    Text("상대방이 나를\n검색할 수 있어요."),
+                                  ],
+                                ),
+                                Icon(Icons.search_rounded),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+            ],
+          )
         ],
       ),
-    );
-  }
-}
-
-class TagInactive extends StatelessWidget {
-  const TagInactive({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text("비활성화"),
     );
   }
 }
