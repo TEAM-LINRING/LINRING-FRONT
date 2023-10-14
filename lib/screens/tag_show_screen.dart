138e3006dc4c5a39dd9c45dab1993d52aea99384
Jiwon Choi <jwchoi@lablup.com>
2023-10-14 17:54:04 +0900

    feat: 태그 아이템 UI 구현 및 API 연결

--- lib/screens/tag_show_screen.dart
+++ lib/screens/tag_show_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:linring_front_flutter/widgets/custom_appbar.dart';
+import 'package:linring_front_flutter/models/tagset_model.dart';
+import 'package:http/http.dart' as http;
 
 class TagShowScreen extends StatefulWidget {
   const TagShowScreen({Key? key}) : super(key: key);
@@ -10,6 +14,31 @@ class TagShowScreen extends StatefulWidget {
 }
 
 class _TagShowScreenState extends State<TagShowScreen> {
+  late Future<List<tagset>> _futureTagsets;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _futureTagsets = _callAPI();
+  }
+
+  Future<List<tagset>> _callAPI() async {
+    // final url = Uri.parse(dotenv.get('API_URL'));
+    final url = Uri.parse('http://127.0.0.1:8000/api/accounts/v2/tagset/');
+    final response = await http.get(url, headers: {
+      'Content-Type': 'application/json',
+    });
+
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(utf8.decode(response.bodyBytes));
+      List<tagset> tagsets =
+          body.map((dynamic e) => tagset.fromJson(e)).toList();
+      return tagsets;
+    } else {
+      throw Exception('Failed to load tagset.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -39,39 +68,99 @@ class _TagShowScreenState extends State<TagShowScreen> {
           Column(
             children: [
               const Text("어떤 친구를 만나게 될까요?"),
-              CarouselSlider(
-                options: CarouselOptions(
-                  height: 300.0,
-                  enableInfiniteScroll: false,
-                ),
-                items: [1, 2, 3, 4, 5].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return const Card(
-                        color: Colors.white,
-                        child: Column(
-                          children: [
-                            Text("북악관에서"),
-                            Text("선배랑"),
-                            Text("스터디하기"),
-                            Text("같이 카페에서 각자 공부할 사람 구해요!"),
-                            Row(
-                              children: [
-                                Column(
+              FutureBuilder(
+                future: _futureTagsets,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text("에러 ${snapshot.error}");
+                  } else if (!snapshot.hasData) {
+                    return const Text("데이터 없음.");
+                  } else {
+                    return CarouselSlider(
+                      options: CarouselOptions(
+                        height: 320.0,
+                        enableInfiniteScroll: false,
+                      ),
+                      items: snapshot.data!.map((tag) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Text("상대방이 나를\n검색할 수 있어요."),
+                                    Text(
+                                      "${tag.place}에서\n${(tag.isSameDepartment) ? "같은 과" : "다른 과"} ${tag.person}랑\n${tag.method}하기",
+                                      style: const TextStyle(fontSize: 24),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      tag.introduction != null
+                                          ? "\"${tag.introduction}\""
+                                          : "",
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Color(0xff999999),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 64,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            const Text(
+                                              "상대방이 나를\n검색할 수 있어요.",
+                                              style: TextStyle(
+                                                fontSize: 11,
+                                                color: Color(0xff999999),
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              width: 80,
+                                              child: FittedBox(
+                                                fit: BoxFit.fill,
+                                                child: CupertinoSwitch(
+                                                  activeColor:
+                                                      const Color(0xff57e554),
+                                                  value: tag.isActive,
+                                                  onChanged: (value) {},
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        const Icon(
+                                          Icons.search_rounded,
+                                          color: Color(0xfffec2b5),
+                                          size: 37,
+                                        ),
+                                      ],
+                                    )
                                   ],
                                 ),
-                                Icon(Icons.search_rounded),
-                              ],
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    );
+                  }
+                },
+              )
             ],
           )
         ],
