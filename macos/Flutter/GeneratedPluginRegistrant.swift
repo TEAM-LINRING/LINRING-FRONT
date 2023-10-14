7e9448cac7a095cca42502af7a09bbb087240955
Jiwon Choi <jwchoi@lablup.com>
2023-10-14 19:44:26 +0900

    chore: `dotenv`, `http` 패키지 추가

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,12 +4,13 @@
 
 import FlutterMacOS
 import Foundation
+
 import firebase_core
 import firebase_messaging
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
