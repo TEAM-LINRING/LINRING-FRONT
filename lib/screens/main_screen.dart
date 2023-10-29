2f0bf4db6428fa854586f0d5755deaefe7f72f02
Jiwon Choi <jwchoi@lablup.com>
2023-10-29 22:17:00 +0900

    feat: `MainScreen`에 종속된 Screen에게 사용자 정보 전달

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:linring_front_flutter/models/login_info.dart';
 import 'package:linring_front_flutter/screens/chat_room_screen.dart';
 import 'package:linring_front_flutter/screens/setting_screen.dart';
@@ -8,7 +6,9 @@ import 'package:linring_front_flutter/screens/tag_show_screen.dart';
 import 'package:linring_front_flutter/widgets/custom_bottom_navigation_bar.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({Key? key}) : super(key: key);
+  final LoginInfo loginInfo;
+
+  const MainScreen(this.loginInfo, {super.key});
 
   @override
   _MainScreenState createState() => _MainScreenState();
@@ -17,38 +17,21 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
 
-  // FlutterSecureStroage 객체를 storage 변수에 할당
-  static const storage = FlutterSecureStorage();
-  // storage에 존재하는 로그인 된 User 정보를 받아올 변수, 공백으로 초기화
-  dynamic loginInfo = '';
-
-  static const List<Widget> _screens = <Widget>[
-    TagShowScreen(),
-    ChatRoomScreen(),
-    SettingScreen(),
-  ];
-
   @override
   void initState() {
     super.initState();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      String? res = await storage.read(key: 'user');
-
-      if (res == null) {
-        // 로그인 만료 및 로그인 필요 안내 출력 후 페이지 이동
-        Navigator.pushNamed(context, '/');
-      } else {
-        // res != null
-        final Map parsed = json.decode(res);
-        loginInfo = LoginInfo.fromJson(parsed);
-      }
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          _buildScreen(0, TagShowScreen(loginInfo: widget.loginInfo)),
+          _buildScreen(1, ChatRoomScreen(loginInfo: widget.loginInfo)),
+          _buildScreen(2, SettingScreen(loginInfo: widget.loginInfo)),
+        ],
+      ),
       bottomNavigationBar: CustomBottomNavigationBar(
         selectedIndex: _selectedIndex,
         onIndexChanged: (index) {
@@ -57,7 +40,14 @@ class _MainScreenState extends State<MainScreen> {
           });
         },
       ),
-      body: _screens[_selectedIndex],
+    );
+  }
+
+  Widget _buildScreen(int index, Widget screen) {
+    return Visibility(
+      visible: _selectedIndex == index,
+      maintainState: true,
+      child: screen,
     );
   }
 }
