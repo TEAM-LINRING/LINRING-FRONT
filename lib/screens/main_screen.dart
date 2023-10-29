a3a6548db23acbe9fefbd6ab639583449e99467b
Jiwon Choi <jwchoi@lablup.com>
2023-10-29 20:58:32 +0900

    feat: `MainScreen` 로그인 사용자 정보 관련 코드 추가
    - 로그인 중인 사용자의 정보를 Flutter Secure Storage에서 가져오도록 코드 추가
    - 저장된 데이터가 없는 경우에는 로그인 페이지로 re-direct 하도록 코드 추가

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:linring_front_flutter/models/login_info.dart';
 import 'package:linring_front_flutter/screens/chat_room_screen.dart';
 import 'package:linring_front_flutter/screens/setting_screen.dart';
 import 'package:linring_front_flutter/screens/tag_show_screen.dart';
@@ -14,11 +17,34 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
 
+  // FlutterSecureStroage 객체를 storage 변수에 할당
+  static const storage = FlutterSecureStorage();
+  // storage에 존재하는 로그인 된 User 정보를 받아올 변수, 공백으로 초기화
+  dynamic loginInfo = '';
+
   static const List<Widget> _screens = <Widget>[
     TagShowScreen(),
     ChatRoomScreen(),
     SettingScreen(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      String? res = await storage.read(key: 'user');
+
+      if (res == null) {
+        // 로그인 만료 및 로그인 필요 안내 출력 후 페이지 이동
+        Navigator.pushNamed(context, '/');
+      } else {
+        // res != null
+        final Map parsed = json.decode(res);
+        loginInfo = LoginInfo.fromJson(parsed);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
