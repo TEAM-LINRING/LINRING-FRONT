9d1002db153868ac423864d3328cb6e370bba8a8
HYEJIN <imagine@kookmin.ac.kr>
2023-11-08 13:53:39 +0900

    Feat: 프로필 확인하기 모달 ui 구현 및 플로우 연결

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -114,6 +114,7 @@ class _ChatScreenState extends State<ChatScreen> {
         suffix: PopupMenuButton<int>(
           onSelected: (int result) {
             if (result == 1) {
+              _showProfileModal(context);
             } else if (result == 2) {
               Navigator.push(
                   context,
@@ -635,6 +636,127 @@ class _ChatScreenState extends State<ChatScreen> {
       asset,
       height: 25,
       width: 25,
+    );
+  }
+
+  void _showProfileModal(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25.0),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+          height: 300,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(
+                      width: 48,
+                      height: 20,
+                    ),
+                    IconButton(
+                      icon: const Icon(
+                        Icons.close_rounded,
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 72,
+                        height: 72,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(
+                            color: const Color(0xffc8c8c8),
+                            width: 0.7,
+                          ),
+                        ),
+                        child: const Center(
+                          child: Image(
+                            image: AssetImage('assets/images/avartar_1.png'),
+                            width: 100,
+                          ),
+                        ),
+                      ),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            child: Text(
+                              '여섯글자이름님',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            child: Text(
+                              '소프트웨어학부 21학번',
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 2,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            child: Text(
+                              '22살 여자 휴학생',
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Text(
+                    '"카페에서 같이 공부할 사람!"',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: CustomOutlinedButton(
+                    label: '프로필 닫기',
+                    backgroundColor: const Color(0xFFFEC2B5),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
