2dea0be3055d92a7ddb0b3ac26b01dbc4dda8f83
Jiwon Choi <jwchoi@lablup.com>
2023-11-08 14:32:54 +0900

    fix: 태그 추가 시 '한줄 소개'를 필수 입력 조건으로 수정

--- lib/screens/tag_add_screen.dart
+++ lib/screens/tag_add_screen.dart
@@ -389,7 +389,12 @@ class _ChoiceLocationState extends State<ChoiceLocation> {
           ),
           CustomTextField(
             textLimit: 30,
-            onChanged: (value) => {introduction = value},
+            onChanged: (value) => {
+              introduction = value,
+              setState(
+                () {},
+              )
+            },
             obscureText: false,
             hintText: "ex. 같이 카페에서 각자 공부할 사람 구해요!",
             padding: const EdgeInsets.all(0.0),
@@ -405,7 +410,9 @@ class _ChoiceLocationState extends State<ChoiceLocation> {
             isActive: (place != null &&
                 department != null &&
                 person != null &&
-                method != null),
+                method != null &&
+                introduction != null &&
+                introduction != ""),
             backgroundColor: const Color(0xfffec2b5),
           )
         ],
