68712ff8233f9d4e4e2e8c99bbfc22ae86766787
HYEJIN <imagine@kookmin.ac.kr>
2023-11-10 18:52:05 +0900

    Design: 닉네임 중복확인 버튼 변경

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -431,74 +431,149 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   height: 0,
                 ),
               )),
-          SizedBox(
-            height: 80,
-            child: Stack(alignment: Alignment.centerRight, children: [
-              CustomTextField(
-                controller: nickNameController,
-                onChanged: (value) {
-                  setState(() {
-                    errorNickName = null;
-                    helperNickName = null;
-                    if (!RegExp(r'^[a-zA-Z0-9가-힣]*$').hasMatch(value)) {
-                      isNickNameValid = false;
-                      errorNickName = '닉네임에 공백이나 특수문자를 사용할 수 없습니다.';
-                    } else if (value.length > 6) {
-                      isNickNameValid = false;
-                      errorNickName = '닉네임은 여섯글자 이내여야 합니다.';
-                    } else {
-                      errorNickName = null;
-                      isNickNameValid = true;
-                    }
-                  });
-                },
-                errorText: errorNickName,
-                hintText: '6글자 이내의 닉네임',
-                obscureText: false,
-                helperText: helperNickName,
-              ),
-              Positioned(
-                  right: 30,
-                  bottom: 30,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                        border: Border(
-                            left: BorderSide(
-                                width: 1, color: Color(0xFFC8AAAA)))),
-                    child: OutlinedButton(
-                        onPressed: () async {
-                          if (isNickNameValid == true) {
-                            bool? result = await _validationNickName(context);
-                            setState(() {
-                              if (result != null) {
-                                if (result) {
-                                  isNickNameUnique = true;
-                                  errorNickName = null;
-                                  helperNickName = '사용 가능한 닉네임입니다.';
-                                } else {
-                                  isNickNameUnique = false;
-                                  helperNickName = null;
-                                  errorNickName = '중복된 닉네임입니다. 다른 닉네임을 사용해주세요.';
-                                }
-                              }
-                              isSignUpButtonEnabled = checkFormValidity();
-                            });
+
+          IntrinsicHeight(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: CustomTextField(
+                      controller: nickNameController,
+                      onChanged: (value) {
+                        setState(() {
+                          errorNickName = null;
+                          helperNickName = null;
+                          if (!RegExp(r'^[a-zA-Z0-9가-힣]*$').hasMatch(value)) {
+                            isNickNameValid = false;
+                            errorNickName = '닉네임에 공백이나 특수문자를 사용할 수 없습니다.';
+                          } else if (value.length > 6) {
+                            isNickNameValid = false;
+                            errorNickName = '닉네임은 여섯글자 이내여야 합니다.';
+                          } else {
+                            errorNickName = null;
+                            isNickNameValid = true;
                           }
-                        },
-                        style: OutlinedButton.styleFrom(
-                            side: BorderSide.none,
-                            padding: const EdgeInsets.symmetric(vertical: 20)),
-                        child: const Text(
-                          '중복 확인',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 13,
-                              fontWeight: FontWeight.w400,
-                              height: 0),
-                        )),
-                  )),
-            ]),
+                        });
+                      },
+                      errorText: errorNickName,
+                      hintText: '6글자 이내의 닉네임',
+                      obscureText: false,
+                      helperText: helperNickName,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 1, 0, 0),
+                      child: OutlinedButton(
+                          onPressed: () async {
+                            if (isNickNameValid == true) {
+                              bool? result = await _validationNickName(context);
+                              setState(() {
+                                if (result != null) {
+                                  if (result) {
+                                    isNickNameUnique = true;
+                                    errorNickName = null;
+                                    helperNickName = '사용 가능한 닉네임입니다.';
+                                  } else {
+                                    isNickNameUnique = false;
+                                    helperNickName = null;
+                                    errorNickName =
+                                        '중복된 닉네임입니다. 다른 닉네임을 사용해주세요.';
+                                  }
+                                }
+                                isSignUpButtonEnabled = checkFormValidity();
+                              });
+                            }
+                          },
+                          style: OutlinedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            backgroundColor: const Color(0xFFFEC2B5),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                side: const BorderSide(
+                                    width: 1, color: Color(0xFFC8AAAA))),
+                          ),
+                          child: const Text(
+                            '중복 확인',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w400,
+                                height: 0),
+                          )),
+                    ),
+                  )
+                ]),
           ),
+          // SizedBox(
+          //   height: 80,
+          //   child: Stack(alignment: Alignment.centerRight, children: [
+          //     CustomTextField(
+          //       controller: nickNameController,
+          //       onChanged: (value) {
+          //         setState(() {
+          //           errorNickName = null;
+          //           helperNickName = null;
+          //           if (!RegExp(r'^[a-zA-Z0-9가-힣]*$').hasMatch(value)) {
+          //             isNickNameValid = false;
+          //             errorNickName = '닉네임에 공백이나 특수문자를 사용할 수 없습니다.';
+          //           } else if (value.length > 6) {
+          //             isNickNameValid = false;
+          //             errorNickName = '닉네임은 여섯글자 이내여야 합니다.';
+          //           } else {
+          //             errorNickName = null;
+          //             isNickNameValid = true;
+          //           }
+          //         });
+          //       },
+          //       errorText: errorNickName,
+          //       hintText: '6글자 이내의 닉네임',
+          //       obscureText: false,
+          //       helperText: helperNickName,
+          //     ),
+          //     Positioned(
+          //         right: 30,
+          //         bottom: 30,
+          //         child: Container(
+          //           decoration: const BoxDecoration(
+          //               border: Border(
+          //                   left: BorderSide(
+          //                       width: 1, color: Color(0xFFC8AAAA)))),
+          //           child: OutlinedButton(
+          //               onPressed: () async {
+          //                 if (isNickNameValid == true) {
+          //                   bool? result = await _validationNickName(context);
+          //                   setState(() {
+          //                     if (result != null) {
+          //                       if (result) {
+          //                         isNickNameUnique = true;
+          //                         errorNickName = null;
+          //                         helperNickName = '사용 가능한 닉네임입니다.';
+          //                       } else {
+          //                         isNickNameUnique = false;
+          //                         helperNickName = null;
+          //                         errorNickName = '중복된 닉네임입니다. 다른 닉네임을 사용해주세요.';
+          //                       }
+          //                     }
+          //                     isSignUpButtonEnabled = checkFormValidity();
+          //                   });
+          //                 }
+          //               },
+          //               style: OutlinedButton.styleFrom(
+          //                   side: BorderSide.none,
+          //                   padding: const EdgeInsets.symmetric(vertical: 20)),
+          //               child: const Text(
+          //                 '중복 확인',
+          //                 style: TextStyle(
+          //                     color: Colors.black,
+          //                     fontSize: 13,
+          //                     fontWeight: FontWeight.w400,
+          //                     height: 0),
+          //               )),
+          //         )),
+          //   ]),
+          // ),
 
           const SizedBox(height: 30),
 
