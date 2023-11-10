63ad2adb05eb60745b272bd6e0752395ffa8c15c
Jiwon Choi <jwchoi@lablup.com>
2023-11-11 00:46:01 +0900

    test: FCM 테스트 코드 작성
    - application background 상태에서 fcm 메세지 수신 확인

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:linring_front_flutter/firebase_options.dart';
@@ -16,6 +17,22 @@ void main() async {
   await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  final fcmToken = await messaging.getToken();
+  print(fcmToken);
+  messaging.onTokenRefresh.listen((fcmToken) {}).onError((err) {});
   runApp(const MyApp());
 }
 
@@ -24,6 +41,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print("enter build");
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      if (message != null) {
+        _handleMessage(context, message);
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      RemoteNotification notification = message.notification!;
+
+      print('Message title: ${notification.title}');
+      print('Message body: ${notification.body}');
+    });
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const EntryScreen(),
@@ -48,5 +79,14 @@ class MyApp extends StatelessWidget {
         return null;
       },
     );
+  }
+
+  void _handleMessage(BuildContext context, RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
   }
 }
