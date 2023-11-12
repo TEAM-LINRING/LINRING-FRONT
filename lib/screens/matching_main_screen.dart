4f57ae2eb59efbcb3395fec3dda3d1885f38d035
HYEJIN <imagine@kookmin.ac.kr>
2023-11-12 20:46:17 +0900

    Feat: 매칭 메인화면 랜덤 캐릭터 배치

--- lib/screens/matching_main_screen.dart
+++ lib/screens/matching_main_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:flutter_svg/svg.dart';
 import 'package:vector_math/vector_math.dart' show radians;
 import 'package:flutter/material.dart';
 
@@ -10,10 +11,12 @@ class MatchingMainScreen extends StatefulWidget {
 }
 
 class _MatchingMainScreenState extends State<MatchingMainScreen> {
-  final radius1 = 100;
-  final radius4 = 150;
-  final radius2 = 200;
-  final radius3 = 300;
+  final Random random = Random();
+
+  int getRandomInt(int max) {
+    return random.nextInt(max);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,76 +28,82 @@ class _MatchingMainScreenState extends State<MatchingMainScreen> {
           ),
         ),
         child: Container(
-            child: Center(
-                child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Image.asset('assets/images/loading.gif'),
-            const SizedBox(
-              height: 5,
+          child: Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset('assets/images/characters/01.svg'),
+                getRandomWidget(3),
+              ],
             ),
-            Transform(
-              transform: Matrix4.identity()
-                ..translate(
-                    radius4 * cos(radians(30)), radius4 * sin(radians(120))),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('assets/images/character/char_pudding.png'),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text('닉네임1')
-                ],
-              ),
-            ),
-            Transform(
-              transform: Matrix4.identity()
-                ..translate(
-                    radius3 * cos(radians(90)), radius3 * sin(radians(60))),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('assets/images/character/char_pudding.png'),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text('닉네임2')
-                ],
-              ),
-            ),
-            Transform(
-              transform: Matrix4.identity()
-                ..translate(
-                    radius2 * cos(radians(250)), radius2 * sin(radians(270))),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('assets/images/character/char_pudding.png'),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text('닉네임3')
-                ],
-              ),
-            ),
-            Transform(
-              transform: Matrix4.identity()
-                ..translate(
-                    radius2 * cos(radians(220)), radius2 * sin(radians(0))),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('assets/images/character/char_pudding.png'),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text('닉네임4')
-                ],
-              ),
-            )
-          ],
-        ))),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getRandomWidget(int count) {
+    int max = 4;
+
+    if (count == 0) {
+      // Condition 0: Do not show anything
+      return Container();
+    } else if (count == 4) {
+      // Condition 4: Show images in all positions
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(max, (index) {
+          return getImageWidget(index);
+        }),
+      );
+    } else {
+      // Conditions 1, 2, 3: Show images in random positions
+      List<int> selectedPositions = [];
+
+      while (selectedPositions.length < count) {
+        int position = getRandomInt(max);
+
+        // Ensure the position is not selected before
+        if (!selectedPositions.contains(position)) {
+          selectedPositions.add(position);
+        }
+      }
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(count, (index) {
+          return getImageWidget(selectedPositions[index]);
+        }),
+      );
+    }
+  }
+
+  Widget getImageWidget(int position) {
+    List<double> translations = [
+      150 * cos(radians(30)),
+      300 * cos(radians(90)),
+      200 * cos(radians(250)),
+      200 * cos(radians(220)),
+    ];
+
+    List<double> verticalTranslations = [
+      150 * sin(radians(120)),
+      300 * sin(radians(55)),
+      200 * sin(radians(270)),
+      200 * sin(radians(0)),
+    ];
+    return Transform(
+      transform: Matrix4.identity()
+        ..translate(translations[position], verticalTranslations[position]),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/characters/char_pudding.png'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text('닉네임')
+        ],
       ),
     );
   }
