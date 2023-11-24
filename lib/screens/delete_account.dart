9cc703d991fc64862e2a5d84352984cd51a0fa02
HYEJIN <imagine@kookmin.ac.kr>
2023-11-24 23:29:07 +0900

    Fix: `탈퇴하기` 기능 버그 해결

--- lib/screens/delete_account.dart
+++ lib/screens/delete_account.dart
@@ -36,7 +36,7 @@ class _DeleteAccountScreenState extends State<DeleteAccountScreen> {
           "password": password,
         }));
     debugPrint((response.statusCode).toString());
-    if (response.statusCode == 204) {
+    if (response.statusCode == 204 || response.statusCode == 200) {
       password = '';
 
       if (!mounted) return;
