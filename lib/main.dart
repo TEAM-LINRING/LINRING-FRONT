d2411bcaa84008d2890cf9869dc24364a48fe5c6
Jiwon Choi <jwchoi@lablup.com>
2023-11-25 20:06:00 +0900

    feat: background message notification 추가 (iOS)
    - 현재 iOS에서 FCM data payload를 통한 background message notification 기능 동작은 확인 완료하였습니다.
    - android에서는 PlatformException이 발생 --> notification에 사용되는 icon이 NullPointer로 인식되어 발생하는 문제로 추정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:linring_front_flutter/firebase_options.dart';
 import 'package:linring_front_flutter/screens/accout_active_screen.dart';
 import 'package:linring_front_flutter/screens/change_password_screen.dart';
@@ -11,13 +13,76 @@ import 'package:linring_front_flutter/screens/selectmajor_screen.dart';
 import 'package:linring_front_flutter/screens/signup_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel',
+  'High Importance Notifications',
+  importance: Importance.high,
+);
+
 @pragma('vm:entry-point')
 void main() async {
   await initializeDateFormatting("ko_KR", null);
   await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(onBackgroundHandler);
+  await _initNotification();
   runApp(const MyApp());
+}
+
+Future<void> _initNotification() async {
+  // 안드로이드 초기 설정
+  const AndroidInitializationSettings initSettingAndroid =
+      AndroidInitializationSettings("mipmap/ic_launcher");
+
+  // iOS 초기 설정
+  const DarwinInitializationSettings initSettingIOS =
+      DarwinInitializationSettings(
+    requestSoundPermission: true,
+    requestBadgePermission: true,
+    requestAlertPermission: true,
+  );
+
+  const InitializationSettings initSettings = InitializationSettings(
+    android: initSettingAndroid,
+    iOS: initSettingIOS,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initSettings);
+}
+
+// Background FCM handler
+Future onBackgroundHandler(RemoteMessage message) async {
+  print('IN BACKGROUND HANDLER');
+  Map<String, dynamic> data = message.data;
+
+  String? title = '새로운 채팅';
+  String? body = '채팅 내용입니다.';
+
+  // if (data['type'] == 1) {
+  //   body = data['message'];
+  // }
+  // if (data['type'] == 2) {
+  //   body = data['args'];
+  // }
+
+  // 알림이 있고, 앱이 백그라운드 상태에서 실행되고 있는 경우에만 알림을 표시
+  flutterLocalNotificationsPlugin.show(
+      message.hashCode,
+      title,
+      body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+        ),
+      ));
+
+  print("END NOTIFICATION SHOWING");
+
+  return Future.value();
 }
 
 class MyApp extends StatelessWidget {
